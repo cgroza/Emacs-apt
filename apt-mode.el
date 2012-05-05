56730a2b663d690a923bf0f1e4051ecc33647c6e
Groza Cristian <cgroza@cgroza-cgroza.(none)>
2012-05-05 18:38:04 -0400

    Created apt-mode functions for major mode.

--- apt-mode.el
+++ apt-mode.el
@@ -6,6 +6,50 @@
   (interactive)
   (apt-search (pkg-at-point)))
 
+(defun apt-mode-download ()
+  (interactive)
+  (apt-download (pkg-at-point)))
+
+(defun apt-mode-change-log ()
+  (interactive)
+  (apt-change-log (pkg-at-point)))
+
+(defun apt-mode-source ()
+  (interactive)
+  (apt-source (pkg-at-point)))
+
+(defun apt-mode-showpkg ()
+  (interactive)
+  (apt-showpkg (pkg-at-point)))
+
+(defun apt-mode-depends ()
+  (interactive)
+  (apt-depends (pkg-at-point)))
+
+(defun apt-mode-rdepends ()
+  (interactive)
+  (apt-rdepends (pkg-at-point)))
+
+(defun apt-mode-pkgnames ()
+  (interactive)
+  (apt-pkgnames (pkg-at-point)))
+
+(defun apt-mode-dotty ()
+  (interactive)
+  (apt-dotty (pkg-at-point)))
+
+(defun apt-mode-xvcg ()
+  (interactive)
+  (apt-xvcg (pkg-at-point)))
+
+(defun apt-mode-policy ()
+  (interactive)
+  (apt-policy (pkg-at-point)))
+
+(defun apt-mode-madison ()
+  (interactive)
+  (apt-madison (pkg-at-point)))
+
 (defvar apt-mode-map 
   (let ((map (make-keymap)))
     (define-key map "\C-j" 'apt-mode-search)
